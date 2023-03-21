f460ccd8b1c3710457d61b593b446221ee1ab554
David Bell <davidgrebell@gmail.com>
2023-03-21 11:15:16 -0700

    switch to fully parallel at slurm level

--- spiking_network_model_22_12_13/submit_2.py
+++ spiking_network_model_22_12_13/submit_2.py
@@ -72,7 +72,7 @@ def pad_zeros(to_pad, length):
 		padded = '0' + padded
 	return padded
 
-batch_size = 8
+batch_size = 1
 
 params = OrderedDict()
 params['SEED'] = [str(i) for i in range(2000, 2005)]
